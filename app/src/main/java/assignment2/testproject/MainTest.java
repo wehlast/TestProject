4c2570794a4e083fa1f7d70bc98337971e41632d
Mads Wehlast Hansen <wehlast@gmail.com>
2017-02-21 09:12:50 +0100

    Final prototype

--- app/src/main/java/assignment2/testproject/MainTest.java
+++ app/src/main/java/assignment2/testproject/MainTest.java
@@ -9,17 +9,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.util.*;
 import java.util.Random;
 
+import static assignment2.testproject.R.id.submit;
+import static assignment2.testproject.R.id.textView6;
 import static assignment2.testproject.R.id.textView5;
 import static assignment2.testproject.R.id.visible;
 
 public class MainTest extends AppCompatActivity {
     int random;
+    EditText answer;
     List<Integer> intlist = new ArrayList<Integer>();
-
+    List<Integer> intlistans = new ArrayList<Integer>();
+    List<Integer> totalint = new ArrayList<Integer>();
+    Button submit;
 
 
     @Override
@@ -28,16 +35,16 @@ public class MainTest extends AppCompatActivity {
         setContentView(R.layout.test);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         Random r = new Random();
-        for(int i = 0; i < 6; i++){
+        for(int i = 0; i < 7; i++){
             random = r.nextInt(9); // [0, 9] + 0 => [0, 9]
             intlist.add(random);
         }
+
         TextView next = new TextView(this);
-        //MyCountDown timer = new MyCountDown(5000, 1000);
         next = (TextView)findViewById(textView5);
         next.setText(String.valueOf(intlist));
-        //timer.start();
         final TextView finalNext = next;
         next.postDelayed(new Runnable() {
             @Override
@@ -46,30 +53,40 @@ public class MainTest extends AppCompatActivity {
             }
         }, 5000);
 
-        
+        //EditText answer = new EditText(this);
+        answer = (EditText)findViewById(R.id.ans);
+       /*
+        */
+        final TextView[] next2 = {new TextView(this)};
+        submit = (Button)findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        String ans1 = answer.getText().toString();
+                        Scanner scanner = new Scanner(ans1);
+                        while (scanner.hasNextInt()) {
+                            intlistans.add(scanner.nextInt());
+                        }
+                        int total = 0;
+                        for (Integer i : intlist) { // assuming list is of type List<Integer>
+                            total = 10*total + i;
+                        }
+                        totalint.add(total);
+
+                        if (totalint.equals(intlistans)){
+
+                            next2[0] = (TextView)findViewById(textView6);
+                            next2[0].setText("Correct");
+                            }else{
+                            next2[0] = (TextView)findViewById(textView6);
+                            next2[0].setText("The correct answer was: " + (String.valueOf(intlist)) + " you answered: "+ (String.valueOf(intlistans)));
+
+                        }
+                    }
+                });
+
 
     }
-
-   /* private class MyCountDown extends CountDownTimer {
-        long duration, interval;
-        public MyCountDown(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-
-            start();
-        }
-
-        @Override
-        public void onFinish() {
-            TextView next = new TextView(this);
-            next = (TextView)findViewById(textView5);
-            textView5.setVisibility(View.INVISIBLE);
-        }
-
-        @Override
-        public void onTick(long duration) {
-            // could set text for a timer here
-        }
-    }*/
-
 
 }
